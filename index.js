691009db932656849c7e86787bd13a4312f83f78
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-11-23 09:23:16 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function saturdayFun(activity="roller-skate") {
+function saturdayFun(activity = 'roller-skate') {
   return `This Saturday, I want to ${activity}!`
-}
+};
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
-}
+const mondayWork = function (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
-  }
-}
+function wrapAdjective(thing1 = '*') {
+  return function (thing2 = 'special') {
+    return `You are ${thing1}${thing2}${thing1}!`;
+  };
+};
+
